d3a76901a11c2778b5e74ad5e59acc044f89824d
maiara.fernandes <{your_email}>
2025-06-04 11:00:34 -0300

    Update Day 4

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,5 +1,10 @@
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 import random
+#option 1
 print(random.choice(friends)) #chooses randomly from a sequence
                               #in this case the list friends
 
+# option 2
+random_index = random.randint(0,4)
+print(friends[random_index]) #chooses a random index from 0 to 4
+                              #and prints the friend at that index
--- Day 4/IndexError/task.py
+++ Day 4/IndexError/task.py
@@ -5,5 +5,18 @@ states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Conne
                      "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado",
                      "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma",
                      "New Mexico", "Arizona", "Alaska", "Hawaii"]
+#print(states_of_america[49])  # This will print "Hawaii", the last state in the list
+#print(states_of_america[50])  # running the line would raise an IndexError because the list has only 50 elements (0-49)
 
-print(states_of_america)
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries",
+               "Pears", "Tomatoes", "Celery", "Potatoes"]
+#note that the list has fruits and vegetables. we can separate them into two lists using nested list
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+#print(dirty_dozen)  # Note that there is two brackets, one for the list of fruits and one for the list of vegetables
+print(dirty_dozen[1][1]) # This will print "Kale", the second vegetable in the second list, in this case the vegetables
+print(dirty_dozen[0][2]) # This will print "Apples", the third fruit in the list of fruits
+#the first bracket is for the list of fruits and vegetables, the second bracket is for the item of the list
